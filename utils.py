035242aa7efd23efc5e2b1c3995da327f30166fd
DimpleB0501 <47828390+DimpleB0501@users.noreply.github.com>
2023-02-11 22:21:00 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -97,7 +97,7 @@ def gradcam_test(dataloader, model, criterion=torch.nn.CrossEntropyLoss()):
     return true,pred,image,true_wrong,pred_wrong
 
 # To plot the wrong predictions given by model
-def gradcam_wrong_plot(n_figures,true,ima,pred,encoder, layer, model):
+def gradcam_wrong_plot(n_figures,true,ima,pred,encoder, layer, model, device = torch.device("cuda" if cuda else "cpu")):
     print('Classes in order Actual and Predicted')
     #n_row = int(n_figures/3)
 
